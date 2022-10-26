4b54b94f2d6535a868e8aa9a95a7b0e9176acc5c
Aleksandr <alexandr44@inbox.lv>
2022-10-26 15:05:14 +0300

    create fighter programm

---
+++ Fighter.py
@@ -0,0 +1,26 @@
+import time
+class Fighter:
+    def __init__(self,life,defense,damage):
+        self.life=life
+        self.defense =defense
+        self.damage =damage
+    def attack(self,fighter):
+        fighter.life -= self._damage -fighter.defense
+fighter_1=Fighter(100,20,40)
+fighter_2=Fighter(100,20,30)   
+
+while True:
+    print("Uzbrūk pirmais cīnītājs")
+    fighter_1.attack(fighter_2)
+    print(f"Dzīvība otrajam cīkstonim: {fighter_2.life}")
+    time.sleep(2)
+    if fighter_2.life<=0:
+        print("F2 zaudēja")
+        break
+    print("Uzbrūk Otrais cīnītājs")
+    fighter_2.attack(fighter_1)
+    print(f"Dzīvība pirmajam cīkstonim: {fighter_1.life}")
+    time.sleep(2)
+    if fighter_1.life<=0:
+        print("F1 zaudēja")
+        break
